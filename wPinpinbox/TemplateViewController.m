c7222f980ebee995072e9d23002dc04ac56cf6c1
appbuilder <appbuilder@shuangyingchuangyide-MacBook-Air.local>
2018-11-09 12:10:12 +0800

    Revert "Rearrange source code directories"
    This reverts commit baabf703ba35644b87d6af958e3c5f5d6e9c4255.

--- wPinpinbox/TemplateViewController.m
+++ wPinpinbox/TemplateViewController.m
@@ -14,7 +14,7 @@
 #import "AsyncImageView.h"
 #import "CooperationViewController.h"
 
-#import "UIColor+Extensions.h"
+#import "UIColor+Extensions.m"
 #import "CustomIOSAlertView.h"
 #import "AlbumCreationViewController.h"
 
